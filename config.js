441435979725894e40ce4b5139aed2f0147a1099
shatir789 <shatir497@gmail.com>
2025-04-22 00:05:46 +0500

    Update config.js

--- config.js
+++ config.js
@@ -173,7 +173,7 @@ for (let i = 0; i < ownerlist.length; i += 2) {
         global.owner.push(owner);
     }
 }
-const defaultOwner = ["639129985130", "DEVELOPER🌹", true];
+const defaultOwner = ["923006838210", "FAISAL DURRANI🌹", true];
 global.owner.push(defaultOwner);
 
 
